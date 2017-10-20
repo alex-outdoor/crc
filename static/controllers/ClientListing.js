fd868ee2e22a87ab4f711a7747667b88d84404fb
alexthi <alexandre.thissen@gmail.com>
2017-10-20 15:08:25 -0700

    collectstatic

---
+++ static/controllers/ClientListing.js
@@ -0,0 +1,22 @@
+app = angular.module("clientListing", ["ngCookies"]);
+app.controller("ClientListing", [
+  "$scope",
+  "$http",
+  "$cookies",
+  function($scope, $http, $cookies) {
+    // Init variables
+    $scope.sortType = "created_date";
+    $scope.sortReverse = true;
+
+    $scope.init = function() {
+      url = "http://" + location.host + "/api/clients/";
+      // API call
+      $http.get(url).success(function(data, status, headers, config) {
+        $scope.data_list = [];
+        for (var i = 0; i < data.length; i++) {
+          $scope.data_list.push(data[i]);
+        }
+      });
+    };
+  }
+]);
